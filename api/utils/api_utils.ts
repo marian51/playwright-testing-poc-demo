947ea7b7eeda277f7374656fa5fc30cab33bf864
Mariusz Jóźwiak <mariusz.jozwiak@onwelo.com>
2024-02-16 09:34:32 +0100

    test: add test for checking if getting store by orderid works correct

--- api/utils/api_utils.ts
+++ api/utils/api_utils.ts
@@ -2,6 +2,7 @@ import { APIRequestContext, APIResponse } from "@playwright/test";
 import { allure } from "allure-playwright";
 import { Attachments } from "../../helpers/textAttachment"
 import { Headers } from "../../helpers/headers";
+import { Params } from "../types/basicTypes";
 
 const attachments = new Attachments();
 const headers = new Headers();
@@ -14,6 +15,15 @@ export class ApiUtils {
     
         allure.attachment("endpoint", endpoint, {contentType: "text/plain"})
         allure.attachment("request", await attachments.requestTextAttachment('POST', endpoint, defaultHeaders, body), {contentType: "text/html"})
+        allure.attachment("response", await attachments.responseTextAttachment(response), {contentType: "text/html"})
+        
+        return response
+    }
+
+    static async get(request: APIRequestContext, endpoint: string, params: Params): Promise<APIResponse> {
+        let response: APIResponse = await request.get(endpoint)
+
+        allure.attachment("endpoint", endpoint, {contentType: "text/plain"})
         allure.attachment("response", await attachments.responseTextAttachment(response), {contentType: "text/html"})
 
         return response
--- tests/api/store/crud.spec.ts
+++ tests/api/store/crud.spec.ts
@@ -1,17 +1,19 @@
-import { APIRequest, APIResponse, expect, test } from '@playwright/test';
+import { APIRequest, APIRequestContext, APIResponse, expect, test } from '@playwright/test';
 import { allure } from 'allure-playwright';
 import { DataGenerator } from '../../../api/utils/generate_data';
 import { ApiUtils } from '../../../api/utils/api_utils';
 import { Store } from '../../../api/types/store';
 import { TestUtils } from '../../utils/test_utils';
-
-const endpoint: string = '/v2/store/order'
-let storeId;
+import { Params } from '../../../api/types/basicTypes';
 
 test.describe("API Store CRUD tests", () => {
+    let storeId: number;
+    let newStoreRequest: Store;
+
     test(`@api Create "Store" in the store`, async ({ request }) => {
+        const endpoint: string = '/v2/store/order'
         let shipDateISO: string;
-        let newStoreRequest: Store;
+        
         let newStoreResponse: APIResponse;
 
         shipDateISO = await DataGenerator.generateDate();
@@ -29,5 +31,17 @@ test.describe("API Store CRUD tests", () => {
         await TestUtils.assertStoreObjectAreEqual(newStoreRequest, newStoreResponse);
     })
 
-    
+    test("@api Read store order by id", async ({ request }) => {
+        const endpoint: string = `/v2/store/order/${storeId}`
+        const params: Params = {
+            "orderId": storeId
+        }
+        
+        const storeResponse: APIResponse = await allure.step("Getting store order by order id", async () => {
+            return await ApiUtils.get(request, endpoint, params)
+        })
+
+        await TestUtils.assertStatusCode(storeResponse);
+        await TestUtils.assertStoreObjectAreEqual(newStoreRequest, storeResponse);
+    })
 })
