a9519e4ca818eec6f5ed566ba84f7aae16aecbb6
Mariusz Jóźwiak <mariusz.jozwiak@onwelo.com>
2024-02-26 08:02:16 +0100

    feat: add basic types
    - HeadersType
    - FormType
    - BasicResponseType

--- api/types/basicTypes.ts
+++ api/types/basicTypes.ts
@@ -1,3 +1,15 @@
-export type Params = { 
+export type ParamsType = { 
     [key: string]: string | number | boolean; 
+}
+
+export type HeadersType = {
+    [key: string]: string;
+}
+
+export type FormType = ParamsType
+
+export type BasicResponseType = {
+    code: number,
+    message: string,
+    type: string
 }
