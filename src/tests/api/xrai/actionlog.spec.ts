5d5382138f4e9221ad4bc183170976099dfe7692
Mariusz Jóźwiak <mariusz.jozwiak@onwelo.com>
2024-02-29 08:36:51 +0100

    refactor: divide basic action log step to test steps

--- src/tests/api/xrai/actionlog.spec.ts
+++ src/tests/api/xrai/actionlog.spec.ts
@@ -1,4 +1,4 @@
-import test from "@playwright/test";
+import test, { APIResponse } from "@playwright/test";
 import { AuthService } from '../../../api/utils/auth_service';
 import { getWithHeaders } from "../../../api/utils/api_utils";
 import { TestUtils } from "../../utils/test_utils";
@@ -6,21 +6,28 @@ import { TestUtils } from "../../utils/test_utils";
 test.describe("API Action Log basic tests", () => {
     test('@action-log Admin user is able to read action logs', async({ request }) => {
         
-        // Zakładając, że użytkownik "Cezary Kruk" z uprawnieniami ADMIN może się autoryzować w API aplikacji
-        const accessToken = await AuthService.getAccessToken(request) 
+        let accessToken: string;
+        let response: APIResponse;
 
-        // Kiedy pobiera metodą GET action logi
-        const endpoint = "/api/dicom-viewer/action_log"
-        const response = await getWithHeaders(request, endpoint, { Authorization: `Bearer ${accessToken}` }, "Getting ActionLogs") 
-
-        // Wtedy endpoint zwraca response o kodzie 200
-        TestUtils.assertStatusCode(response, 200)
-
-        // I payload responsa zawiera listę action logów
-        // I liczba wyświetlonych action logów jest równa 25
-        TestUtils.assertResponseHasProperCountObjects((await response.json()).response, 25)
-
-        // I lista wyświetlonych action logów jest posortowana po polu creationTime w kierunku "DESC"
-        TestUtils.assertObjectsAreSortedByDateTime((await response.json()).response, 'creationTime', 'DESC')
+        await test.step('Zakładając, że użytkownik "Cezary Kruk" z uprawnieniami ADMIN może się autoryzować w API aplikacji', async () => {
+            accessToken = await AuthService.getAccessToken(request) 
+        })
+        
+        await test.step('Kiedy pobiera metodą GET action logi', async () => {
+            const endpoint = "/api/dicom-viewer/action_log"
+            response = await getWithHeaders(request, endpoint, { Authorization: `Bearer ${accessToken}` }, "Getting ActionLogs") 
+        })
+        
+        await test.step('Wtedy endpoint zwraca response o kodzie 200', () => {
+            TestUtils.assertStatusCode(response, 200)
+        })
+        
+        await test.step('I payload responsa zawiera listę action logów\nI liczba wyświetlonych action logów jest równa 25', async () => {
+            TestUtils.assertResponseHasProperCountObjects((await response.json()).response, 25)
+        })
+        
+        await test.step('I lista wyświetlonych action logów jest posortowana po polu creationTime w kierunku "DESC"', async () => {
+            TestUtils.assertObjectsAreSortedByDateTime((await response.json()).response, 'creationTime', 'DESC')
+        })       
     })
 })
