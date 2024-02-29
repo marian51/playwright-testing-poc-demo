02742808a7c9585b613dfc745a0bf7f2e81acb78
Mariusz Jóźwiak <mariusz.jozwiak@onwelo.com>
2024-02-29 13:41:08 +0100

    test: add test for checking if getting action logs with user without institution works correct
    - added users file
    - added method for getting user param
    - added test method
    - formated institutions file

--- src/tests/api/xrai/actionlog.spec.ts
+++ src/tests/api/xrai/actionlog.spec.ts
@@ -59,4 +59,32 @@ test.describe("API Action Log basic tests", () => {
             TestAssertions.assertObjectsAreSortedByDateTime((await response.json()).response, 'creationTime', 'DESC')
         })    
     })
+
+    test('@action-log Użytkownik administracyjny może odczytać action logi użytkownika bez podania instytucji', async ({ request }) => {
+        let accessToken: string;
+        let response: APIResponse;
+        const userFullName: string = "Anna Urban"
+        
+        await test.step('Zakładając, że użytkownik "Cezary Kruk" z uprawnieniami ADMIN może się autoryzować w API aplikacji', async () => {
+            accessToken = await AuthService.getAccessToken(request) 
+        }) 
+
+        await test.step(`Kiedy pobiera metodą GET action logi użytkownika "${userFullName}"`, async () => {
+            const endpoint = "/api/dicom-viewer/action_log"
+            const filters: string = await TestUtils.prepareFilters({ userUuid: TestUtils.getUserParam(userFullName, "uuid") })
+            response = await getWithHeaders(request, endpoint + filters, { Authorization: `Bearer ${accessToken}` }, "Getting ActionLogs") 
+        })
+
+        await test.step('Wtedy endpoint zwraca response o kodzie 200', () => {
+            TestAssertions.assertStatusCode(response, 200)
+        })
+
+        await test.step('I payload responsa zawiera listę action logów dla instytucji\nI liczba wyświetlonych action logów jest równa 25', async () => {
+            TestAssertions.assertResponseHasProperCountObjects((await response.json()).response, 25)
+        })
+
+        await test.step('I lista wyświetlonych action logów jest posortowana po polu creationTime w kierunku "DESC"', async () => {
+            TestAssertions.assertObjectsAreSortedByDateTime((await response.json()).response, 'creationTime', 'DESC')
+        })   
+    })
 })
--- src/tests/utils/test_utils.ts
+++ src/tests/utils/test_utils.ts
@@ -1,11 +1,16 @@
 import { ParamsType } from "../../api/types/basicTypes";
 import institutions from "../../resources/institutions.json"
+import users from "../../resources/users.json";
 
 export class TestUtils {
 
     static getInstitutionParam(institutionAlias: string, param: string): string {
         const institution = institutions[institutionAlias]
         return institution[param]
+    }
+
+    static getUserParam(userFullName: string, param: string): string {
+        return users[userFullName][param]
     }
 
     static async prepareFilters(params: ParamsType): Promise<string> {
