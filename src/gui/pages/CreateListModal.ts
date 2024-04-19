39af6bc389ab29969b302e1d9d51e355a78faad8
Mariusz Jóźwiak <mariusz.jozwiak@int.pl>
2024-04-19 08:24:12 +0200

    test: add test for checking if creating new list works correct

---
+++ src/gui/pages/CreateListModal.ts
@@ -0,0 +1,49 @@
+import test, { Locator, Page, expect } from "@playwright/test";
+import { CommonMethods } from "../common_methods";
+import CustomReporter from "../../helpers/reporter";
+import { allure } from "allure-playwright";
+
+export class CreateListModal {
+    readonly page: Page;
+    readonly modalView: Locator
+    readonly newListNameInput: Locator
+    readonly createListButton: Locator
+
+    constructor(page: Page) {
+        this.page = page;
+        this.modalView = this.page.locator("cu-simple-input-modal")
+        this.newListNameInput = this.page.getByPlaceholder("List")
+        this.createListButton = this.page.locator("[data-test=simple-input-modal__button]")
+    }
+
+    async typeIntoNewListNameInput(text: string) {
+        return await test.step(`Typing '${text}' into new list name input`, async () => {
+            await CommonMethods.typeIntoField(this.newListNameInput, text)
+            CustomReporter.logAction(`Typed '${text}' into new list name input`)
+            await allure.attachment("screenshot.png", await this.page.screenshot(), {
+                contentType: "image/png"
+            })
+        })
+    }
+
+    async clickOnCreateListButton() {
+        return await test.step(`Clicking on 'Create List' button on modal`, async () => {
+            await CommonMethods.clickOnElement(this.createListButton)
+            CustomReporter.logAction(`Clicked on 'Create List' button on modal`)
+            await allure.attachment("screenshot.png", await this.page.screenshot(), {
+                contentType: "image/png"
+            })
+        })
+    }
+
+    async waitForModalDisappear() {
+        return await test.step(`Waiting for modal disappear`, async () => {
+            await expect(this.modalView).toBeHidden()
+            CustomReporter.logAction(`Modal window has disappeared`)
+            await allure.attachment("screenshot.png", await this.page.screenshot(), {
+                contentType: "image/png"
+            })
+        })
+    }
+
+}
--- src/gui/pages/MainView.ts
+++ src/gui/pages/MainView.ts
@@ -1,15 +1,19 @@
 import { Locator, Page } from "@playwright/test";
 import CustomReporter from "../../helpers/reporter";
+import { CommonMethods } from "../common_methods";
+import { allure } from "allure-playwright";
 
 export class MainView {
     readonly page: Page
     readonly newTaskInput: Locator
     readonly newListButton: Locator
+    readonly addListButton: Locator
 
     constructor(page: Page) {
         this.page = page
         this.newTaskInput = page.locator("[data-test=task-row-new__input]")
         this.newListButton = page.getByText(" New List ")
+        this.addListButton = page.getByText(" Add list ")
     }
 
     async waitForNewTaskInput() {
@@ -19,6 +23,16 @@ export class MainView {
     async waitForNewListButton() {
         await this.newListButton.waitFor()
         CustomReporter.logAction(`Main view has been refreshed`)
+    }
+
+    async clickOnAddListButton() {
+        return allure.step("Clicking on 'Add list' button", async () => {
+            await CommonMethods.clickOnElement(this.addListButton)
+            CustomReporter.logAction(`Clicked on 'Add list' button on main view`)
+            await allure.attachment("screenshot.png", await this.page.screenshot(), {
+                contentType: "image/png"
+            })
+        })
     }
 
 }
