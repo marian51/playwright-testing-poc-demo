d19c9ef1b1d52d4ecc69c867b1567bdbf1621e9b
Mariusz Jóźwiak <mariusz.jozwiak@int.pl>
2024-04-24 11:35:14 +0200

    feat: add page class for delete list modal

---
+++ src/gui/pages/DeleteListModal.ts
@@ -0,0 +1,32 @@
+import test, { Locator, Page } from "@playwright/test";
+import { CommonMethods } from "../common_methods";
+import CustomReporter from "../../helpers/reporter";
+import { allure } from "allure-playwright";
+
+export class DeleteListModal {
+    readonly page: Page;
+    readonly modalView: Locator;
+    readonly deleteButton: Locator;
+
+    constructor(page: Page) {
+        this.page = page;
+        this.modalView = this.page.locator("cu-subcategory-confirm-delete");
+        this.deleteButton = this.page.locator(
+            "[data-test=confirmation-modal__confirm-button]"
+        );
+    }
+
+    async clickOnDeleteButton() {
+        return await test.step(`Clicking on 'Delete' button`, async () => {
+            await CommonMethods.clickOnElement(this.deleteButton);
+            CustomReporter.logAction(`Clicked on 'Delete' button`);
+            await allure.attachment(
+                "screenshot.png",
+                await this.page.screenshot(),
+                {
+                    contentType: "image/png",
+                }
+            );
+        });
+    }
+}
