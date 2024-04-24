c97f7c9ff8f207c80ea680efb33215c81b9a14c1
Mariusz Jóźwiak <mariusz.jozwiak@int.pl>
2024-04-24 11:00:43 +0200

    refactor: format file with Prettier

--- src/helpers/hooks.ts
+++ src/helpers/hooks.ts
@@ -1,23 +1,41 @@
 import { APIRequestContext } from "@playwright/test";
-import { spaceDeleteEndpoint, spaceEndpoint } from "../api/endpoints/clickUp_endpoints";
+import {
+    spaceDeleteEndpoint,
+    spaceEndpoint,
+} from "../api/endpoints/clickUp_endpoints";
 import { AuthService } from "../api/utils/auth_service";
 
 export class Hooks {
-    static async deleteSpaceByName(request: APIRequestContext, spaceName: string) {
-        const teamId = process.env.BASE_TEAM_ID as string
-        const endpoint = spaceEndpoint.replace('TEAM_ID', teamId)
-        const apiKey = AuthService.getApiKey()
-        
-        const spacesResponse = await request.get(endpoint, { headers: { Authorization: apiKey } })
-        const id = (await spacesResponse.json()).spaces.filter(r => r.name === spaceName)[0].id
-        await request.delete(spaceDeleteEndpoint + id, { headers: { Authorization: apiKey }})
+    static async deleteSpaceByName(
+        request: APIRequestContext,
+        spaceName: string
+    ) {
+        const teamId = process.env.BASE_TEAM_ID as string;
+        const endpoint = spaceEndpoint.replace("TEAM_ID", teamId);
+        const apiKey = AuthService.getApiKey();
+
+        const spacesResponse = await request.get(endpoint, {
+            headers: { Authorization: apiKey },
+        });
+        const id = (await spacesResponse.json()).spaces.filter(
+            (r) => r.name === spaceName
+        )[0].id;
+        await request.delete(spaceDeleteEndpoint + id, {
+            headers: { Authorization: apiKey },
+        });
     }
 
-    static async createSpaceByName(request: APIRequestContext, spaceName: string) {
-        const teamId = process.env.BASE_TEAM_ID as string
-        const endpoint = spaceEndpoint.replace('TEAM_ID', teamId)
-        const apiKey = AuthService.getApiKey()
+    static async createSpaceByName(
+        request: APIRequestContext,
+        spaceName: string
+    ) {
+        const teamId = process.env.BASE_TEAM_ID as string;
+        const endpoint = spaceEndpoint.replace("TEAM_ID", teamId);
+        const apiKey = AuthService.getApiKey();
 
-        await request.post(endpoint, { headers: { Authorization: apiKey }, data: { name: spaceName } })
+        await request.post(endpoint, {
+            headers: { Authorization: apiKey },
+            data: { name: spaceName },
+        });
     }
 }
