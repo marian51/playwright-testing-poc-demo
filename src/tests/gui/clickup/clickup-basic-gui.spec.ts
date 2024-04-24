1f29fec23d183f0b6829e4d6dff6dff405ad79a1
Mariusz Jóźwiak <mariusz.jozwiak@int.pl>
2024-04-24 13:15:31 +0200

    feat: add new test for checking if adding new task works correct (and format file with Prettier)

--- src/tests/gui/clickup/clickup-basic-gui.spec.ts
+++ src/tests/gui/clickup/clickup-basic-gui.spec.ts
@@ -20,10 +20,7 @@ test.describe("GUI Clickup basic functionalities tests", () => {
         });
     });
 
-    test("@gui-clickup @clickup Create space and check if space is created", async ({
-        page,
-        request,
-    }) => {
+    test("@gui-clickup @clickup Create space and check if space is created", async ({ page, request }) => {
         await allure.tag("GUI");
         await allure.tag("Space");
 
@@ -60,18 +57,13 @@ test.describe("GUI Clickup basic functionalities tests", () => {
         });
 
         await test.step("Then new space is created", async () => {
-            await leftSideBar.assertThatLeftSideBarContainsElement(
-                newSpaceName
-            );
+            await leftSideBar.assertThatLeftSideBarContainsElement(newSpaceName);
         });
 
         await Hooks.deleteSpaceByName(request, newSpaceName);
     });
 
-    test("@gui-clickup @clickup Delete space and check if space is deleted", async ({
-        page,
-        request,
-    }) => {
+    test("@gui-clickup @clickup Delete space and check if space is deleted", async ({ page, request }) => {
         await allure.tag("GUI");
         await allure.tag("Space");
 
@@ -107,16 +99,11 @@ test.describe("GUI Clickup basic functionalities tests", () => {
         });
 
         await test.step("Then new space is deleted", async () => {
-            await leftSideBar.assertThatLeftSideBarDoesNotContainElement(
-                newSpaceName
-            );
+            await leftSideBar.assertThatLeftSideBarDoesNotContainElement(newSpaceName);
         });
     });
 
-    test("@gui-clickup @clickup Create list and check if list is created", async ({
-        page,
-        request,
-    }) => {
+    test("@gui-clickup @clickup Create list and check if list is created", async ({ page, request }) => {
         await allure.tag("GUI");
         await allure.tag("List");
 
@@ -144,7 +131,7 @@ test.describe("GUI Clickup basic functionalities tests", () => {
         });
 
         await test.step("And there is space created by user", async () => {
-            await leftSideBar.checkIfElementExist(newSpaceName);
+            await leftSideBar.checkIfElementExist(newSpaceName, true);
         });
 
         await test.step("When user created new list in existing space", async () => {
@@ -164,10 +151,7 @@ test.describe("GUI Clickup basic functionalities tests", () => {
         await Hooks.deleteSpaceByName(request, newSpaceName);
     });
 
-    test("@gui-clickup @clickup Delete list and check if list is deleted", async ({
-        page,
-        request,
-    }) => {
+    test("@gui-clickup @clickup Delete list and check if list is deleted", async ({ page, request }) => {
         await allure.tag("GUI");
         await allure.tag("List");
 
@@ -183,11 +167,7 @@ test.describe("GUI Clickup basic functionalities tests", () => {
         await allure.parameter("User password", password, { mode: "masked" });
 
         const spaceId = await Hooks.createSpaceByName(request, newSpaceName);
-        const listId = await Hooks.createListByName(
-            request,
-            spaceId,
-            newListName
-        );
+        const listId = await Hooks.createListByName(request, spaceId, newListName);
 
         await test.step("Given user can log in to the application", async () => {
             await loginPage.goto();
@@ -209,9 +189,58 @@ test.describe("GUI Clickup basic functionalities tests", () => {
         });
 
         await test.step("Then new list is deleted", async () => {
-            await leftSideBar.assertThatLeftSideBarDoesNotContainElement(
-                newListName
-            );
+            await leftSideBar.assertThatLeftSideBarDoesNotContainElement(newListName);
+        });
+
+        await Hooks.deleteSpaceById(request, spaceId);
+    });
+
+    test("@gui-clickup @clickup Create task and check if task is created", async ({ page, request }) => {
+        await allure.tag("GUI");
+        await allure.tag("Task");
+
+        const newSpaceName = "GUI space create test";
+        const newListName = "GUI list create test";
+        const newTaskName = "GUI task create test";
+        const username = process.env.USERNAME as string;
+        const password = process.env.PASSWORD as string;
+        const loginPage = new LoginPage(page);
+        const leftSideBar = new LeftSideBar(page);
+        const mainView = new MainView(page);
+
+        await allure.parameter("User name", username);
+        await allure.parameter("User password", password, { mode: "masked" });
+
+        const spaceId = await Hooks.createSpaceByName(request, newSpaceName);
+        await Hooks.createListByName(request, spaceId, newListName);
+
+        await test.step("Given user can log in to the application", async () => {
+            await loginPage.goto();
+            await loginPage.typeIntoEmailField(username);
+            await loginPage.typeIntoPasswordField(password);
+            await loginPage.clickLogIn();
+
+            await mainView.waitForExcelCsvBubble();
+        });
+
+        await test.step("And there is space created by user", async () => {
+            await leftSideBar.checkIfElementExist(newSpaceName, true);
+            await leftSideBar.clickOnElement(newSpaceName);
+        });
+
+        await test.step("And there is list created by user", async () => {
+            await leftSideBar.checkIfElementExist(newListName, false);
+            await leftSideBar.clickOnElement(newListName);
+        });
+
+        await test.step("When user creates new task in existing list", async () => {
+            await mainView.clickOnNewTaskInput();
+            await mainView.typeIntoNewTaskInput(newTaskName);
+            await mainView.clickOnSaveTaskButton();
+        });
+
+        await test.step("Then new list is deleted", async () => {
+            await mainView.assertThatTaskIsDisplayedOnTheList(newTaskName);
         });
 
         await Hooks.deleteSpaceById(request, spaceId);
