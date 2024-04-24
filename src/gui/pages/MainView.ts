d3faeac9b256ef77de08309d5cf6372eae08a9a2
Mariusz Jóźwiak <mariusz.jozwiak@int.pl>
2024-04-24 13:14:40 +0200

    feat: add some methods to main view class
    - Added methods fro clicking and typing into new task input
    - Added method for clicking on 'Save' task button
    - Added assertion method for checking if new task is displayed on list

--- src/gui/pages/MainView.ts
+++ src/gui/pages/MainView.ts
@@ -1,4 +1,4 @@
-import { Locator, Page } from "@playwright/test";
+import { Locator, Page, expect } from "@playwright/test";
 import CustomReporter from "../../helpers/reporter";
 import { CommonMethods } from "../common_methods";
 import { allure } from "allure-playwright";
@@ -9,15 +9,15 @@ export class MainView {
     readonly newListButton: Locator;
     readonly addListButton: Locator;
     readonly excelCsvBubble: Locator;
+    readonly saveTaskButton: Locator;
 
     constructor(page: Page) {
         this.page = page;
         this.newTaskInput = page.locator("[data-test=task-row-new__input]");
         this.newListButton = page.getByText(" New List ");
         this.addListButton = page.getByText(" Add list ");
-        this.excelCsvBubble = page.locator(
-            "[data-pendo=csv-integration-bubble]"
-        );
+        this.excelCsvBubble = page.locator("[data-pendo=csv-integration-bubble]");
+        this.saveTaskButton = page.getByRole("button", { name: "Save" });
     }
 
     async waitForNewTaskInput() {
@@ -33,29 +33,56 @@ export class MainView {
         return allure.step("Waiting for 'Excel & CSV' bubble", async () => {
             await this.excelCsvBubble.waitFor();
             CustomReporter.logAction(`'Excel & CSV' bubble has been loaded`);
-            await allure.attachment(
-                "screenshot.png",
-                await this.page.screenshot(),
-                {
-                    contentType: "image/png",
-                }
-            );
+            await allure.attachment("screenshot.png", await this.page.screenshot(), {
+                contentType: "image/png",
+            });
         });
     }
 
     async clickOnAddListButton() {
         return allure.step("Clicking on 'Add list' button", async () => {
             await CommonMethods.clickOnElement(this.addListButton);
-            CustomReporter.logAction(
-                `Clicked on 'Add list' button on main view`
-            );
-            await allure.attachment(
-                "screenshot.png",
-                await this.page.screenshot(),
-                {
-                    contentType: "image/png",
-                }
-            );
+            CustomReporter.logAction(`Clicked on 'Add list' button on main view`);
+            await allure.attachment("screenshot.png", await this.page.screenshot(), {
+                contentType: "image/png",
+            });
+        });
+    }
+
+    async clickOnNewTaskInput() {
+        return allure.step("Clicking on new task input", async () => {
+            await CommonMethods.clickOnElement(this.newTaskInput);
+            CustomReporter.logAction("Clicked on new task input");
+            await allure.attachment("screenshot.png", await this.page.screenshot(), {
+                contentType: "image/png",
+            });
+        });
+    }
+
+    async clickOnSaveTaskButton() {
+        return allure.step("Clicking on 'Save' task button", async () => {
+            await CommonMethods.clickOnElement(this.saveTaskButton);
+            CustomReporter.logAction("Clicked on 'Save' task input");
+            await allure.attachment("screenshot.png", await this.page.screenshot(), {
+                contentType: "image/png",
+            });
+        });
+    }
+
+    async typeIntoNewTaskInput(text: string) {
+        return allure.step("Typing into new task input", async () => {
+            await CommonMethods.typeIntoField(this.newTaskInput, text);
+            CustomReporter.logAction(`Typed 'text' into new task input`);
+            await allure.attachment("screenshot.png", await this.page.screenshot(), {
+                contentType: "image/png",
+            });
+        });
+    }
+
+    async assertThatTaskIsDisplayedOnTheList(taskName: string) {
+        return allure.step(`Checking if task with name ${taskName} is displayed on the list`, async () => {
+            const element = this.page.locator(`[data-test="task-row-main__link-text__${taskName}"]`);
+            await expect(element).toBeVisible();
         });
     }
 }
