25ec7d79f0555c69fb01cb18f877d5cd81c93b08
Mariusz Jóźwiak <mariusz.jozwiak@int.pl>
2024-04-24 13:34:17 +0200

    refactor: again format file with Prettier

--- src/helpers/hooks.ts
+++ src/helpers/hooks.ts
@@ -1,16 +1,9 @@
 import { APIRequestContext } from "@playwright/test";
-import {
-    listCreateEndpoint,
-    spaceDeleteEndpoint,
-    spaceEndpoint,
-} from "../api/endpoints/clickUp_endpoints";
+import { listCreateEndpoint, spaceDeleteEndpoint, spaceEndpoint } from "../api/endpoints/clickUp_endpoints";
 import { AuthService } from "../api/utils/auth_service";
 
 export class Hooks {
-    static async deleteSpaceByName(
-        request: APIRequestContext,
-        spaceName: string
-    ) {
+    static async deleteSpaceByName(request: APIRequestContext, spaceName: string) {
         const teamId = process.env.BASE_TEAM_ID as string;
         const endpoint = spaceEndpoint.replace("TEAM_ID", teamId);
         const apiKey = AuthService.getApiKey();
@@ -18,9 +11,7 @@ export class Hooks {
         const spacesResponse = await request.get(endpoint, {
             headers: { Authorization: apiKey },
         });
-        const id = (await spacesResponse.json()).spaces.filter(
-            (r) => r.name === spaceName
-        )[0].id;
+        const id = (await spacesResponse.json()).spaces.filter((r) => r.name === spaceName)[0].id;
         await request.delete(spaceDeleteEndpoint + id, {
             headers: { Authorization: apiKey },
         });
@@ -36,10 +27,7 @@ export class Hooks {
         });
     }
 
-    static async createSpaceByName(
-        request: APIRequestContext,
-        spaceName: string
-    ): Promise<string> {
+    static async createSpaceByName(request: APIRequestContext, spaceName: string): Promise<string> {
         const teamId = process.env.BASE_TEAM_ID as string;
         const endpoint = spaceEndpoint.replace("TEAM_ID", teamId);
         const apiKey = AuthService.getApiKey();
@@ -52,11 +40,7 @@ export class Hooks {
         return (await response.json()).id;
     }
 
-    static async createListByName(
-        request: APIRequestContext,
-        spaceId: string,
-        listName: string
-    ): Promise<string> {
+    static async createListByName(request: APIRequestContext, spaceId: string, listName: string): Promise<string> {
         const teamId = process.env.BASE_TEAM_ID as string;
         const endpoint = listCreateEndpoint.replace("SPACE_ID", spaceId);
         const apiKey = AuthService.getApiKey();
