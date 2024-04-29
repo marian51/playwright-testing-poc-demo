f9e2c7fe90e4b219c66590eca02bc843e71310a7
Mariusz Jóźwiak <mariusz.jozwiak@int.pl>
2024-04-29 12:11:56 +0200

    test: add test for checking if error message is displayed while passing incorrect email

--- src/tests/gui/clickup/clickup-login.spec.ts
+++ src/tests/gui/clickup/clickup-login.spec.ts
@@ -45,4 +45,34 @@ test.describe("GUI ClickUp basic test on login screen", async () => {
             await mainView.assertThatMainViewIsLoaded();
         });
     });
+
+    test("@gui-clickup @clickup @login Check if passing incorrect email returns proper message", async ({ page }) => {
+        allure.tag("GUI");
+        allure.tag("Login");
+
+        const notCorrectUsername = "notcorrect@email.com";
+        const properErrorMessage = "Email not found. Click here to create an account!";
+        const loginPage = new LoginPage(page);
+
+        await allure.addParameter("Incorrect user name", notCorrectUsername);
+
+        await test.step("Given login view of application is opened", async () => {
+            await loginPage.goto();
+            await loginPage.checkIfMainHeaderIsDisplayed();
+            await loginPage.checkIfLoginButtonIsDisplayed();
+        });
+
+        await test.step("When user passes incorrect email to email input", async () => {
+            await loginPage.typeIntoEmailField(notCorrectUsername);
+            await loginPage.mainHeader.focus();
+        });
+
+        await test.step("Then the error message is displayed under email input", async () => {
+            await loginPage.assertThatErrorMessageUnderEmailInputIsDisplayed();
+        });
+
+        await test.step("And the error message has proper test", async () => {
+            await loginPage.assertThatErrorEmailMessageHasProperText(properErrorMessage);
+        });
+    });
 });
